3b28a6cbdb8a030cce6f85aa294c98730eb37e20
Iqbal <iqbalhunowu15@gmail.com>
2026-01-19 16:26:56 +0800

    19-01-2024

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+session_unset();
+session_destroy();
+
+include "config/konfigurasi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="asset/css/backgroundindex.css">
+    <script src="asset/js/jquery-3.7.1.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="kontener">
+        <div class="kartu">
+            asd
+        </div>
+    </div>
+</body>
+</html>
