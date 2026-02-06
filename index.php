83ba0b52adaa9b9972ff52591842b37e9615587a
Iqbal <iqbalhunowu15@gmail.com>
2026-02-06 14:30:16 +0800

    Perubahan dari PC-4, LabKom1

--- index.php
+++ index.php
@@ -10,15 +10,25 @@ include "config/konfigurasi.php";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="asset/css/backgroundindex.css">
+    <link rel="stylesheet" href="asset/css/bindex.css">
     <script src="asset/js/jquery-3.7.1.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Asrama Beringin Jaya</title>
 </head>
 <body>
+    <div class="flower f1">❀</div>
+    <div class="flower f2">❀</div>
+    <div class="flower f3">❀</div>
+    <div class="flower f4">❀</div>
     <div class="kontener">
+        <h1>Asrama Putri Beringin Jaya</h1>
         <div class="kartu">
-            asd
+            <h2>Login</h2>
+            <h4>Username</h4>
+            <input type="text">
+            <h4>Password</h4>
+            <input type="password">
+            <button >Login</button>
         </div>
     </div>
 </body>
