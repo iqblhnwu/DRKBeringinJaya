3fb7fb181649dcc2d852d980401d87acec02fd54
Reja Aswad Podungge <rezaaswad03@gmail.com>
2026-02-06 16:35:04 +0800

    06-02-2026, Tampilan Login dan Fungsi

---
+++ config/proses.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include "konfigurasi.php";
+$conf = $_POST['konf'];
+
+if($conf == "login"){
+    $user = addslashes($_POST['vUser']);
+    $pass = addslashes($_POST['vPass']);
+    $sql = "SELECT * FROM akun WHERE nama='$user' AND passmd5=MD5('$pass')";
+    $query = mysqli_query($koneksi, $sql);
+
+    if(mysqli_num_rows($query) >= 1){
+        $data = mysqli_fetch_assoc($query);
+        if($data['peran'] == "Administrator"){
+            $_SESSION['lvl'] = $data['peran'];
+            echo $data['peran'];
+        }else if($data['peran'] == "Penyewa" ){
+            $_SESSION['lvl'] = $data['peran'];
+            echo $data['peran'];
+        }else{
+            echo "404";
+        }
+    }else{
+        echo "Username atau Password salah!";
+    }
+}
+?>
+
--- index.php
+++ index.php
@@ -10,15 +10,71 @@ include "config/konfigurasi.php";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="asset/css/backgroundindex.css">
+    <link rel="stylesheet" href="asset/css/bgindex.css">
     <script src="asset/js/jquery-3.7.1.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Asrama Beringin Jaya</title>
 </head>
+
 <body>
+    <script>
+        $(document).ready(function(){
+            $('#log').on('click',function(e){
+                e.preventDefault();
+                let tUser = $('#user').val();
+                let tPass = $('#pass').val();
+                let konfirmasi = "login";
+                if(tUser == "" && tPass == ""){
+                    $('#user').focus();
+                }else if(tUser == ""){
+                    $('#user').focus();
+                }else if(tPass == ""){
+                    $('#pass').focus();
+                }else{
+                    $.ajax({
+                        url: "config/proses.php",
+                        type: "POST",
+                        dataType:'html',
+                        data:{
+                            vUser: tUser,
+                            vPass: tPass,
+                            konf: konfirmasi
+                        },
+                        success: function(v){
+                            val = v.trim();
+                            if(val == "Administrator"){
+                                window.location.href = "administrator/admin_mainpage.php";
+                                $('#user').val("");
+                                $('#pass').val("");
+                            }else if(val == "Penyewa"){
+                                window.location.href = "user/user_mainpage.php";
+                                $('#user').val("");
+                                $('#pass').val("");
+                            }else{
+                                alert(val);
+                                $('#user').val("");
+                                $('#pass').val("");
+                            }
+                        }
+                    })
+                }
+            })
+        });
+    </script>
+    <div class="navbar"></div>
     <div class="kontener">
-        <div class="kartu">
-            asd
+        <div class="flower f1">❀</div>
+        <div class="flower f2">❀</div>
+        <h1 class="judul">Asrama Putri Beringin Jaya</h1>
+        <div class="flower f3">❀</div>
+        <div class="flower f4">❀</div>
+        <div class="card">
+            <h2>Login</h2>
+            <h4>Username</h4>
+            <input type="text" id="user" placeholder="-">
+            <h4>Password</h4>
+            <input type="password" id="pass" placeholder="-">
+            <button type="submit" id="log">Login</button>
         </div>
     </div>
 </body>
